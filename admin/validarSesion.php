1fac0124f736420858c3a5b8022345a24ab180db
PaulaTaborda7 <ptabordam@unal.edu.co>
2022-01-08 11:34:50 -0500

    Se borraron comentarios

--- admin/validarSesion.php
+++ admin/validarSesion.php
@@ -1,11 +1,5 @@
 <?php
     require("../database.php");
-    // Si ya hay una sesión y el usuario quiere volver al index, no lo dejo y lo redirijo al crud
-    // if(basename($_SERVER["REQUEST_URI"], ".php") == "index"){
-    //     if(isset($_SESSION['admin']['id']) && isset($_SESSION['admin']['correo'])){
-    //         header("location: crud.php");
-    //     }
-    // }
 
     if(!isset($_SESSION['admin']['id']) || !isset($_SESSION['admin']['correo'])){
         $_SESSION['fail'] = "Debe ingresar con sus credenciales para poder acceder a esta vista.";
